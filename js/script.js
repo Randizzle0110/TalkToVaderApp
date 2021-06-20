59301da7674056746158dae8d556d9cc2cf3ada3
Randizzle0110 <Randyraber21@YAHOO.com>
2021-06-19 23:57:50 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,7 +15,26 @@ simpleAnswers[3] = "Ofcourse.";
 simpleAnswers[4] = "Probally not.";
 simpleAnswers[5] = "Doubtfull.";
 
+var simpleAnswers2= [];
 
+simpleAnswers2[0] = "Good";
+simpleAnswers2[1] = "Bad";
+simpleAnswers2[2] = "horrible";
+simpleAnswers2[3] = "not bad.";
+simpleAnswers2[4] = "not good";
+simpleAnswers2[5] = "great.";
+
+var quesWords = [];
+
+	quesWords[0] = "will";
+	quesWords[1] = "should";
+	quesWords[2] = "can";
+	quesWords[3] = "are";
+	quesWords[4] = "did";
+	quesWords[5] = "could";
+	quesWords[6] = "would";
+	
+	
 var button = document.querySelector("#button");
 button.addEventListener("click", clickHandler, false);
 button.style.cursor = "pointer";
@@ -34,16 +53,57 @@ var output = document.querySelector("#output");
 var input = document.querySelector("#textbox");
 var randomNumber = Math.floor(Math.random() * 5);
 input = input.value.toLowerCase();
-console.log(input.value);
 
-if ( (input.indexOf("will") >=0) || (input.indexOf("should") >=0)|| (input.indexOf("can") >=0)){
+
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[0]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[1]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[2]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[3]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[4]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[5]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+if ( (input.indexOf("how") >=0) && (input.indexOf(quesWords[6]) >=0)){
+var randomNumber = Math.floor(Math.random() * 6);
+	output.innerHTML = simpleAnswers2[randomNumber];
+clear();
+}
+else if ( (input.indexOf(quesWords[0]) >=0) || (input.indexOf(quesWords[1]) >=0)|| (input.indexOf(quesWords[2]) >=0)|| (input.indexOf(quesWords[3]) >=0)
+|| (input.indexOf(quesWords[4]) >=0)|| (input.indexOf(quesWords[5]) >=0)|| (input.indexOf(quesWords[6]) >=0)){
 	var randomNumber = Math.floor(Math.random() * 6);
 	output.innerHTML = simpleAnswers[randomNumber];
-	newImg();
 	clear();
 }
+
 else if ( (input.indexOf("day") >=0)){
 output.innerHTML = "this will be a day long rememebered";
+clear();
+}
+else if ( (input.indexOf("armor") >=0)){
+var str = "Click here for more";
+output.innerHTML = "Too much to tell " + str.link("http://starwars.wikia.com/wiki/Darth_Vader%27s_armor");
 clear();
 }
 
@@ -51,6 +111,9 @@ else{
 output.innerHTML = replies[randomNumber];
 	clear();
 	 }
+
+
+
 
 }
 	
